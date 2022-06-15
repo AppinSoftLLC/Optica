b3b7f75eb1bbf56f59f98d4df94b510b601a59cd
Davka81 <j.davka@gmail.com>
2022-06-15 19:48:47 +0800

    update

--- src/Controller/ReportController.php
+++ src/Controller/ReportController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\RoomsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,10 +16,41 @@ class ReportController extends AbstractController
     /**
      * @Route("/", name="report")
      */
-    public function device(): Response
+    public function device(RoomsRepository $roomsRepository): Response
     {
         return $this->render('report/report.html.twig', [
-            'controller_name' => 'ReportController',
+            'rooms' => $roomsRepository->findByAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/single", name="report_single")
+     */
+    public function single(RoomsRepository $roomsRepository): Response
+    {
+        return $this->render('report/report.html.twig', [
+            'rooms' => $roomsRepository->findByAll(),
+        ]);
+    }
+    
+    /**
+     * @Route("/single", name="report_all")
+     */
+    public function all(RoomsRepository $roomsRepository): Response
+    {
+        return $this->render('report/report.html.twig', [
+            'rooms' => $roomsRepository->findByAll(),
+        ]);
+    }
+    
+    /**
+     * @Route("/single", name="report_abnormal")
+     */                
+    
+    public function abnormal(RoomsRepository $roomsRepository): Response
+    {
+        return $this->render('report/report.html.twig', [
+            'rooms' => $roomsRepository->findByAll(),
         ]);
     }
 }
